bf8c2abee1649e751e229085eb89accac335a4d5
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2020-12-17 17:01:54 -0500

    Completed part 2 of day 17

--- AdventOfCode/Source/Challenges/Challenge_17.cpp
+++ AdventOfCode/Source/Challenges/Challenge_17.cpp
@@ -12,7 +12,7 @@ std::string const CChallenge_17::sm_inputFilePath = "Inputs/Input_Challenge_17.t
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 EErrorCode CChallenge_17::SetUp_FirstPart()
 {
-    return LoadInitialState();
+    return LoadInitialState(false);
 }
 
 EErrorCode CChallenge_17::Run_FirstPart()
@@ -54,12 +54,35 @@ EErrorCode CChallenge_17::CleanUp_FirstPart()
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 EErrorCode CChallenge_17::SetUp_SecondPart()
 {
-    return LoadInitialState();
+    return LoadInitialState(true);
 }
 
 EErrorCode CChallenge_17::Run_SecondPart()
 {
-    return EErrorCode::NotImplemented;
+    static size_t const cycleCount = 6;
+
+    if (DRAW_DEBUGS)
+    {
+        std::cout << "Initial state" << std::endl << std::endl;
+        DrawCubeGrid();
+        std::cout << std::endl << std::endl;
+    }
+
+    size_t activeCubeCount = 0;
+    for (size_t currentCycle = 1; currentCycle <= cycleCount; ++currentCycle)
+    {
+        activeCubeCount = CycleCubeGrid();
+
+        std::cout << "Cycle #" << currentCycle << std::endl << std::endl;
+        if (DRAW_DEBUGS)
+        {
+            DrawCubeGrid();
+            std::cout << std::endl << std::endl;
+        }
+    }
+
+    std::cout << "Active cubeCount = " << activeCubeCount << std::endl;
+    return EErrorCode::Success;
 }
 
 EErrorCode CChallenge_17::CleanUp_SecondPart()
@@ -71,9 +94,9 @@ EErrorCode CChallenge_17::CleanUp_SecondPart()
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-EErrorCode CChallenge_17::LoadInitialState()
+EErrorCode CChallenge_17::LoadInitialState(bool const is4D)
 {
-    static size_t const gridSizeMultiplier = 7;
+    static size_t const extraGridSize = 16;
 
     std::vector<std::string> lines;
     EErrorCode const readErrorCode = FileHelper::ReadLines(sm_inputFilePath, lines);
@@ -82,108 +105,126 @@ EErrorCode CChallenge_17::LoadInitialState()
         return readErrorCode;
     }
 
-    size_t const inputGridSizeX = lines[0].size();
-    size_t const inputGridSizeY = lines.size();
-    size_t const inputGridSize = gridSizeMultiplier * (inputGridSizeY > inputGridSizeX ? inputGridSizeY : inputGridSizeX);
-    CreateCubeGrid(inputGridSize);
+    size_t const inputGridSizeX = extraGridSize + lines[0].size();
+    size_t const inputGridSizeY = extraGridSize + lines.size();
+    size_t const inputGridSizeZ = extraGridSize + (inputGridSizeY > inputGridSizeX ? inputGridSizeY : inputGridSizeX);
+    size_t const inputGridSizeW = is4D ? inputGridSizeZ : 1;
+    CreateCubeGrid(inputGridSizeX, inputGridSizeY, inputGridSizeZ, inputGridSizeW);
 
-    size_t const anchorX = (inputGridSize - inputGridSizeX) / 2;
-    size_t const anchorY = (inputGridSize - inputGridSizeY) / 2;
-    size_t const anchorZ = (inputGridSize - 1) / 2;
+    size_t const anchorX = (inputGridSizeX - lines[0].size() - 1) / 2 + 1;
+    size_t const anchorY = (inputGridSizeY - lines.size() - 1) / 2 + 1;
+    size_t const anchorZ = (inputGridSizeZ - 1) / 2 + 1;
+    size_t const anchorW = (inputGridSizeW - 1) / 2 + 1;
 
-    for (size_t y = 0; y < inputGridSizeY; ++y)
+    for (size_t y = 0; y < lines.size(); ++y)
     {
         std::string const& line = lines[y];
 
-        for (size_t x = 0; x < inputGridSizeX; ++x)
+        for (size_t x = 0; x < lines[0].size(); ++x)
         {
             bool const isCubeActive = line[x] == '#';
-            m_cubeGrid[anchorZ][anchorY + y][anchorX + x] = isCubeActive;
-            m_altCubeGrid[anchorZ][anchorY + y][anchorX + x] = isCubeActive;
+            m_cubeGrid[anchorW][anchorZ][anchorY + y][anchorX + x] = isCubeActive;
+            m_altCubeGrid[anchorW][anchorZ][anchorY + y][anchorX + x] = isCubeActive;
         }
     }
 
     return EErrorCode::Success;
 }
 
-void CChallenge_17::CreateCubeGrid(size_t const gridSize)
+void CChallenge_17::CreateCubeGrid(size_t const sizeX, size_t const sizeY, size_t const sizeZ, size_t const sizeW)
 {
-    m_cubeGrid = new bool**[gridSize];
-    m_altCubeGrid = new bool**[gridSize];
+    m_cubeSizeX = sizeX + 2;
+    m_cubeSizeY = sizeY + 2;
+    m_cubeSizeZ = sizeZ + 2;
+    m_cubeSizeW = sizeW + 2;
 
-    for (size_t z = 0; z < gridSize; ++z)
+    m_cubeGrid = new bool***[m_cubeSizeW];
+    m_altCubeGrid = new bool***[m_cubeSizeW];
+
+    for (size_t w = 0; w < m_cubeSizeW; ++w)
     {
-        m_cubeGrid[z] = new bool*[gridSize];
-        m_altCubeGrid[z] = new bool*[gridSize];
+        m_cubeGrid[w] = new bool**[m_cubeSizeZ];
+        m_altCubeGrid[w] = new bool**[m_cubeSizeZ];
 
-        for (size_t y = 0; y < gridSize; ++y)
+        for (size_t z = 0; z < m_cubeSizeZ; ++z)
         {
-            m_cubeGrid[z][y] = new bool[gridSize];
-            m_altCubeGrid[z][y] = new bool[gridSize];
+            m_cubeGrid[w][z] = new bool*[m_cubeSizeY];
+            m_altCubeGrid[w][z] = new bool*[m_cubeSizeY];
 
-            for (size_t x = 0; x < gridSize; ++x)
+            for (size_t y = 0; y < m_cubeSizeY; ++y)
             {
-                m_cubeGrid[z][y][x] = false;
-                m_altCubeGrid[z][y][x] = false;
+                m_cubeGrid[w][z][y] = new bool[m_cubeSizeX];
+                m_altCubeGrid[w][z][y] = new bool[m_cubeSizeX];
+
+                for (size_t x = 0; x < m_cubeSizeX; ++x)
+                {
+                    m_cubeGrid[w][z][y][x] = false;
+                    m_altCubeGrid[w][z][y][x] = false;
+                }
             }
         }
     }
-
-    m_cubeGridSize = gridSize;
 }
 
 void CChallenge_17::DestroyCubeGrid()
 {
-    for (size_t z = 0; z < m_cubeGridSize; ++z)
+    for (size_t w = 0; w < m_cubeSizeW; ++w)
     {
-        for (size_t y = 0; y < m_cubeGridSize; ++y)
+        for (size_t z = 0; z < m_cubeSizeZ; ++z)
         {
-            delete[] m_cubeGrid[z][y];
-            delete[] m_altCubeGrid[z][y];
+            for (size_t y = 0; y < m_cubeSizeY; ++y)
+            {
+                delete[] m_cubeGrid[w][z][y];
+                delete[] m_altCubeGrid[w][z][y];
+            }
+
+            delete[] m_cubeGrid[w][z];
+            delete[] m_altCubeGrid[w][z];
         }
 
-        delete[] m_cubeGrid[z];
-        delete[] m_altCubeGrid[z];
+        delete[] m_cubeGrid[w];
+        delete[] m_altCubeGrid[w];
     }
 
     delete[] m_cubeGrid;
     delete[] m_altCubeGrid;
-
-    m_cubeGridSize = 0;
 }
 
 size_t CChallenge_17::CycleCubeGrid()
 {
     size_t activeCubeCount = 0;
 
-    for (size_t z = 1; z < m_cubeGridSize - 1; ++z)
+    for (size_t w = 1; w < m_cubeSizeW - 1; ++w)
     {
-        for (size_t y = 1; y < m_cubeGridSize - 1; ++y)
+        for (size_t z = 1; z < m_cubeSizeZ - 1; ++z)
         {
-            for (size_t x = 1; x < m_cubeGridSize - 1; ++x)
+            for (size_t y = 1; y < m_cubeSizeY - 1; ++y)
             {
-                bool cubeState = m_cubeGrid[z][y][x];
-                size_t const activeNeighbourCount = CountActiveNeighbours(x, y, z);
+                for (size_t x = 1; x < m_cubeSizeX - 1; ++x)
+                {
+                    bool cubeState = m_cubeGrid[w][z][y][x];
+                    size_t const activeNeighbourCount = CountActiveNeighbours(x, y, z, w);
 
-                if (cubeState)
-                {
-                    if (activeNeighbourCount < 2 || activeNeighbourCount > 3)
+                    if (cubeState)
                     {
-                        cubeState = false;
+                        if (activeNeighbourCount < 2 || activeNeighbourCount > 3)
+                        {
+                            cubeState = false;
+                        }
                     }
-                }
-                else
-                {
-                    if (activeNeighbourCount == 3)
+                    else
                     {
-                        cubeState = true;
+                        if (activeNeighbourCount == 3)
+                        {
+                            cubeState = true;
+                        }
                     }
-                }
 
-                m_altCubeGrid[z][y][x] = cubeState;
-                if (cubeState)
-                {
-                    ++activeCubeCount;
+                    m_altCubeGrid[w][z][y][x] = cubeState;
+                    if (cubeState)
+                    {
+                        ++activeCubeCount;
+                    }
                 }
             }
         }
@@ -193,21 +234,24 @@ size_t CChallenge_17::CycleCubeGrid()
     return activeCubeCount;
 }
 
-size_t CChallenge_17::CountActiveNeighbours(size_t const anchorX, size_t const anchorY, size_t const anchorZ) const
+size_t CChallenge_17::CountActiveNeighbours(size_t const anchorX, size_t const anchorY, size_t const anchorZ, size_t const anchorW) const
 {
     size_t activeNeighbourCount = 0;
 
-    for (size_t z = anchorZ - 1; z <= anchorZ + 1; ++z)
+    for (size_t w = anchorW - 1; w <= anchorW + 1; ++w)
     {
-        for (size_t y = anchorY - 1; y <= anchorY + 1; ++y)
+        for (size_t z = anchorZ - 1; z <= anchorZ + 1; ++z)
         {
-            for (size_t x = anchorX - 1; x <= anchorX + 1; ++x)
+            for (size_t y = anchorY - 1; y <= anchorY + 1; ++y)
             {
-                if (x != anchorX || y != anchorY || z != anchorZ)
+                for (size_t x = anchorX - 1; x <= anchorX + 1; ++x)
                 {
-                    if (m_cubeGrid[z][y][x])
+                    if (x != anchorX || y != anchorY || z != anchorZ || w != anchorW)
                     {
-                        ++activeNeighbourCount;
+                        if (m_cubeGrid[w][z][y][x])
+                        {
+                            ++activeNeighbourCount;
+                        }
                     }
                 }
             }
@@ -219,16 +263,20 @@ size_t CChallenge_17::CountActiveNeighbours(size_t const anchorX, size_t const a
 
 void CChallenge_17::DrawCubeGrid() const
 {
-    for (size_t y = 0; y < m_cubeGridSize; ++y)
+    for (size_t w = 1; w < m_cubeSizeW - 1; ++w)
     {
-        for (size_t z = 0; z < m_cubeGridSize; ++z)
+        for (size_t y = 1; y < m_cubeSizeY - 1; ++y)
         {
-            for (size_t x = 0; x < m_cubeGridSize; ++x)
+            for (size_t z = 1; z < m_cubeSizeZ - 1; ++z)
             {
-                char const stateSymbol = m_cubeGrid[z][y][x] ? '#' : '.';
-                std::cout << stateSymbol;
+                for (size_t x = 1; x < m_cubeSizeX - 1; ++x)
+                {
+                    char const stateSymbol = m_cubeGrid[w][z][y][x] ? '#' : '.';
+                    std::cout << stateSymbol;
+                }
+                std::cout << "  ";
             }
-            std::cout << "  ";
+            std::cout << std::endl;
         }
         std::cout << std::endl;
     }
--- AdventOfCode/Source/Challenges/Challenge_17.h
+++ AdventOfCode/Source/Challenges/Challenge_17.h
@@ -13,15 +13,18 @@ private:
     virtual EErrorCode Run_SecondPart() override;
     virtual EErrorCode CleanUp_SecondPart() override;
 
-    EErrorCode LoadInitialState();
-    void CreateCubeGrid(size_t const gridSize);
+    EErrorCode LoadInitialState(bool const is4D);
+    void CreateCubeGrid(size_t const sizeX, size_t const sizeY, size_t const sizeZ, size_t const sizeW);
     void DestroyCubeGrid();
     size_t CycleCubeGrid();
-    size_t CountActiveNeighbours(size_t const anchorX, size_t const anchorY, size_t const anchorZ) const;
+    size_t CountActiveNeighbours(size_t const anchorX, size_t const anchorY, size_t const anchorZ, size_t const anchorW) const;
     void DrawCubeGrid() const;
 
     static std::string const sm_inputFilePath;
-    bool*** m_cubeGrid = nullptr;
-    bool*** m_altCubeGrid = nullptr;
-    size_t m_cubeGridSize = 0;
+    bool**** m_cubeGrid = nullptr;
+    bool**** m_altCubeGrid = nullptr;
+    size_t m_cubeSizeX = 0;
+    size_t m_cubeSizeY = 0;
+    size_t m_cubeSizeZ = 0;
+    size_t m_cubeSizeW = 0;
 };
